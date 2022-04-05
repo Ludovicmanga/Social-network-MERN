8d864a841f5fd92567648c9c6cf036fc928283ed
Ludovic <ludovic.mangaj@gmail.com>
2022-04-05 14:54:32 +0200

    the signup system is complete

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const userRoutes = require('./routes/user.routes');
 require('dotenv').config({path: './config/.env'});
 require('./config/db');
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
+app.use('/api/user', userRoutes);
 
 app.listen(PORT, () => {
     console.log(`le serveur est lancé sur le port ${PORT}`);
