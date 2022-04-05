7d6e5126f9cb6c5b51cf0c06370751377f1ab0ce
Ludovic <ludovic.mangaj@gmail.com>
2022-04-05 16:49:46 +0200

    the whole CRUD is working

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,6 @@
+const req = require("express/lib/request");
 const UserModel = require("../models/user.model");
 const ObjectId = require('mongoose').Types.ObjectId;
-const User = require('../models/user.model')
 
 module.exports.getAllUsers = async (req, res) => {
     const users = await UserModel.find().select('-password')
@@ -17,12 +17,21 @@ module.exports.userInfo = (req, res) => {
     }).select('-password');
 }
 
-module.exports.updateUser = async (req, res) => {
+module.exports.updateUser = (req, res) => {
     if (!ObjectId.isValid(req.params.id))
       return res.status(400).send("ID unknown : " + req.params.id);
       const userObject = {...req.body};
   
-      User.updateOne({ _id: req.params.id }, { ...userObject, _id: req.params.id })
+      UserModel.updateOne({ _id: req.params.id }, { ...userObject, _id: req.params.id })
       .then(() => res.status(200).json({ message: 'Objet modifié !'}))
       .catch(error => res.status(400).json({ error }));
-  };
+}
+
+module.exports.deleteUser = (req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+      return res.status(400).send("ID unknown : " + req.params.id);
+
+    UserModel.deleteOne({_id: req.params.id})
+        .then(() => res.status(202).json({ message: 'Objet supprimé !'}))
+        .catch(error => res.status(400).json({ error }))
+}
