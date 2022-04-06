2915eb7245dbc6a4bf1f1945c8245930a8327323
Ludovic <ludovic.mangaj@gmail.com>
2022-04-06 12:47:59 +0200

    the checkUser and requireAuth are working

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,39 @@
+const jwt = require('jsonwebtoken');
+const UserModel = require('../models/user.model');
+
+module.exports.checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if(token) {
+        jwt.verify(token, process.env.TOKEN_SECRET, async( error, decodedToken) => {
+            if (error) {
+                res.locals.user = null;
+                res.cookie('jwt', '', {maxAge: 1});
+                next()
+            } else {
+                let user = await UserModel.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        })
+    } else {
+        res.locals.user = null;
+    }
+}
+
+module.exports.requireAuth = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        jwt.verify(token, process.env.TOKEN_SECRET, async (error, decodedToken) => {
+            if(error) {
+                console.log(error);
+            } else {
+                console.log(decodedToken.id);
+                next();
+            }
+        })
+    } else {
+        console.log('no token');
+    }
+}
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 require('dotenv').config({path: './config/.env'});
 require('./config/db');
-
+const {checkUser, requireAuth} = require('./middleware/auth.middleware');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
+app.get('*', checkUser);
+app.get('/jwtid', requireAuth, (req, res) => {});
 app.use('/api/user', userRoutes);
 
 app.listen(PORT, () => {
