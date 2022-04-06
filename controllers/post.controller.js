9375f9056ca4ba7639dffc9d5ff32ecb92c7442e
Ludovic <ludovic.mangaj@gmail.com>
2022-04-06 17:37:38 +0200

    the post CRUD is complete

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -23,7 +23,24 @@ module.exports.createPost = (req, res) => {
 }
 
 module.exports.updatePost = (req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+      return res.status(400).send("ID unknown : " + req.params.id);
+
+      const updatedRecord = {
+          message: req.body.message
+      }
+      console.log(updatedRecord)
+
+      PostModel.updateOne({_id: req.params.id}, {$set: updatedRecord} )
+        .then(() => res.status(200).json({mesage: "post modifié"}))
+        .catch(error => res.status(200).json({ error }))
 }
 
 module.exports.deletePost = (req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+      return res.status(400).send("ID unknown : " + req.params.id);
+
+    PostModel.deleteOne({_id: req.params.id})
+        .then(() => res.status(202).json({ message: 'Post supprimé !'}))
+        .catch(error => res.status(400).json({ error }))
 }
