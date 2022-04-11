7035e7667a59e0332ca45128926486bc9e29d66c
Ludovic <ludovic.mangaj@gmail.com>
2022-04-11 15:32:09 +0200

    we made the navbar and started to make it dynamic

--- client/src/components/Log/SignUpForm.js
+++ client/src/components/Log/SignUpForm.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import SignInForm from './SignInForm';
 
 export default function SignUpForm() {
+  const [formSubmit, setFormSubmit] = useState(false);
   const [pseudo, setPseudo] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -39,6 +41,8 @@ export default function SignUpForm() {
             pseudoError.innerHTML = res.data.formattedErrors.pseudo;
             emailError.innerHTML = res.data.formattedErrors.email;
             passwordError.innerHTML = res.data.formattedErrors.password;
+          } else {
+            setFormSubmit(() => true);
           }
         })
         .catch(error => console.log(error))
@@ -46,54 +50,64 @@ export default function SignUpForm() {
   }
 
   return (
-    <form action="" onSubmit={handleRegister} id="sign-up-form">
-      <label htmlFor="pseudo">Pseudo</label>
-      <br />
-      <input
-        type="text"
-        name="pseudo"
-        id="pseudo"
-        onChange={ (e) => setPseudo(e.target.value)}
-        value={pseudo}
-      />
-      <div className="pseudo error"></div>
-      <br />
-      <label htmlFor="email">Email</label>
-      <br />
-      <input type="text"
-        name="email"
-        id="mail"
-        onChange={ (e) => setEmail(e.target.value)}
-        value={email}
-      />
-      <div className="email error"></div>
-      <br />
-      <label htmlFor="password">Mot de passe</label>
-      <br />
-      <input
-        type="password"
-        name="password"
-        id="password"
-        onChange={ (e) => setPassword(e.target.value)}
-        value={password}
-      />  
-      <div className="password error"></div>
-      <br />
-      <label htmlFor="password-conf">Confirmer le mot de passe</label>
-      <br />
-      <input
-        type="password"
-        name="password"
-        id="password-conf"
-        onChange={ (e) => setControlPassword(e.target.value)}
-        value={controlPassword} 
-      />
-      <div className="password-confirm error"></div>
-      <br />
-      <input type="checkbox" id="terms"/>
-      <label htmlFor="terms">J'accepte les <a href="/" target="_blank" rel="noopener noreferrer">conditions générales</a></label>
-      <div className="terms error"></div>
-      <input type="submit" value="valider inscription" />
-    </form>
+    <>
+      { formSubmit ? (
+        <>
+          <SignInForm />
+          <span></span>
+          <h4 className='success'>Enregistrement réussi, veuillez vous connecter</h4>
+        </> 
+      ):
+        <form action="" onSubmit={handleRegister} id="sign-up-form">
+          <label htmlFor="pseudo">Pseudo</label>
+          <br />
+          <input
+            type="text"
+            name="pseudo"
+            id="pseudo"
+            onChange={ (e) => setPseudo(e.target.value)}
+            value={pseudo}
+          />
+          <div className="pseudo error"></div>
+          <br />
+          <label htmlFor="email">Email</label>
+          <br />
+          <input type="text"
+            name="email"
+            id="mail"
+            onChange={ (e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <div className="email error"></div>
+          <br />
+          <label htmlFor="password">Mot de passe</label>
+          <br />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={ (e) => setPassword(e.target.value)}
+            value={password}
+          />  
+          <div className="password error"></div>
+          <br />
+          <label htmlFor="password-conf">Confirmer le mot de passe</label>
+          <br />
+          <input
+            type="password"
+            name="password"
+            id="password-conf"
+            onChange={ (e) => setControlPassword(e.target.value)}
+            value={controlPassword} 
+          />
+          <div className="password-confirm error"></div>
+          <br />
+          <input type="checkbox" id="terms"/>
+          <label htmlFor="terms">J'accepte les <a href="/" target="_blank" rel="noopener noreferrer">conditions générales</a></label>
+          <div className="terms error"></div>
+          <input type="submit" value="valider inscription" />
+        </form>
+      }
+    </>
   )
 }
