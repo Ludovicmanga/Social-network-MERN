83c1c2e78c20b7c187c80b3caa220e3bd932a185
Ludovic <ludovic.mangaj@gmail.com>
2022-04-14 14:20:15 +0200

    the video posting system works

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UidContext } from '../components/AppContext'
 import LeftNav from '../components/LeftNav'
+import Log from '../components/Log';
+import NewPostForm from '../components/Post/NewPostForm';
 import Thread from '../components/Thread'
 
 export default function Home() {
+  const uid = useContext(UidContext);
+
   return (
     <div className='home'>
       <LeftNav />
       <div className='main'>
+        <div className='home-header'>
+        {uid ? (
+          <NewPostForm />
+        ) : <Log signin={true} signup={false} />}
+        </div>
         <Thread />
       </div>
     </div>
